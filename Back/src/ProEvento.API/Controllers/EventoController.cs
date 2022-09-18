555de88963e888d7d1825080c641cdfdadaa5e2a
vinicius freitas silva <vinicius.freitas@inflor.com.br>
2022-09-18 13:25:39 -0300

    Seção 1

---
+++ Back/src/ProEvento.API/Controllers/EventoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEvento.API.Models;
+
+namespace ProEvento.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController() 
+        { 
+            
+        }
+        public IEnumerable<Evento> evento = new Evento[]
+        {
+            new Evento()
+             {
+                EventoId = 1,
+                Tema = "Angular + .NET 5",
+                Local = "Alagoinhas - BAHIA",
+                Lote = "Primeiro Lote",
+                QuantidadePessoas = 100,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "foto.png"
+             },
+            new Evento()
+             {
+                EventoId = 2,
+                Tema = "React + .NET 5",
+                Local = "Alagoinhas - BAHIA",
+                Lote = "Segundo Lote",
+                QuantidadePessoas = 200,
+                DataEvento = DateTime.Now.AddDays(3).ToString(),
+                ImagemURL = "foto.png"
+             }
+        };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return evento;
+        }
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo Post";
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return evento.Where(e => e.EventoId == id);
+        }
+    }
+}
