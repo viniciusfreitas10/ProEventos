b56f0ce1915363552d7dc92745dd45b1e4a24eb6
vinicius freitas silva <vinicius.freitas@inflor.com.br>
2022-09-19 22:21:32 -0300

    Seção Angular

--- Back/src/ProEvento.API/Startup.cs
+++ Back/src/ProEvento.API/Startup.cs
@@ -32,6 +32,7 @@ namespace ProEvento.API
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))  
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEvento.API", Version = "v1" });
@@ -54,6 +55,10 @@ namespace ProEvento.API
 
             app.UseAuthorization();
 
+            app.UseCors(cors => cors.AllowAnyHeader().
+                AllowAnyMethod().
+                AllowAnyOrigin()
+            );
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
