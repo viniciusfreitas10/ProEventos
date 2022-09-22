f0ee99659748755e01740a71d7629f3dbdcc5f07
vinicius freitas silva <vinicius.freitas@inflor.com.br>
2022-09-22 12:13:11 -0300

    Alterações no front

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,17 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrado:any = [];
+  public ImageWidth = 100;
+  public ImageMargin = 10;
+  isCollapsed = true;
+  ExibirImagem: boolean = true;
+  private _filtroListaPaginaInicial: string = '';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos()
   }
-
+  public get filtroListaPaginaInicial(): string{
+    return this._filtroListaPaginaInicial;
+  }
+  public set filtroListaPaginaInicial(NewFiltroListaPaginaInicial: string ){
+    this._filtroListaPaginaInicial = NewFiltroListaPaginaInicial;
+    this.eventosFiltrado = this.filtroListaPaginaInicial ? this.FiltrarEventos(this.filtroListaPaginaInicial) : this.eventos;
+  }
+  public FiltrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleUpperCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleUpperCase().indexOf(filtrarPor) !== -1 ||
+       evento.local.toLocaleUpperCase().indexOf(filtrarPor) !== - 1 || evento.dataEvento.toLocaleUpperCase().indexOf(filtrarPor) !== -1
+    )
+  }
+  public controleDeExibicaoDeImagem(): void{
+    this.ExibirImagem = !this.ExibirImagem;
+  }
   public getEventos(): void{
     this.http.get("https://localhost:5001/api/Evento/").subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrado = response
+      },
       error => console.log(error),
     );
   }
